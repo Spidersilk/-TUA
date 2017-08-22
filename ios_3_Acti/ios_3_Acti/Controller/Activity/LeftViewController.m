9acce73ccc34d099fbed70e839e3a04f61dcb229
PengJinPing <2468814340@qq.com>
2017-08-22 17:03:50 +0800

    登录页

--- ios_3_Acti/ios_3_Acti/Controller/Activity/LeftViewController.m
+++ ios_3_Acti/ios_3_Acti/Controller/Activity/LeftViewController.m
@@ -8,14 +8,22 @@
 
 #import "LeftViewController.h"
 
-@interface LeftViewController ()
-
+@interface LeftViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+- (IBAction)loginAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)settingAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+@property (strong,nonatomic) NSArray *arr;
 @end
 
 @implementation LeftViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self uiLayout];
+    [self dataInitialize];
     // Do any additional setup after loading the view.
 }
 
@@ -23,6 +31,47 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)uiLayout{
+    _avatarImageView.layer.borderColor = [[UIColor lightGrayColor]CGColor];
+}
+- (void)dataInitialize{
+    _arr = @[@"我的活动",@"我的推广",@"积分中心",@"意见反馈",@"关于我们"];
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if(section == 0){
+        return  _arr.count;
+    }else{
+        return 1;
+    }
+    return _arr.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+   
+    if(indexPath.section == 0){
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MemberCell" forIndexPath:indexPath];
+        cell.textLabel.text = _arr[indexPath.row];
+        return cell;
+    }else{
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EmptyCell" forIndexPath:indexPath];
+        return cell;
+    }
+}
+//设置cell的高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if(indexPath.section == 0){
+        return 50.f;
+    }else{
+        return UI_SCREEN_H - 500;
+    }
+    
+}
+//按住细胞以后（取消选择）
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+   }
 
 /*
 #pragma mark - Navigation
@@ -34,4 +83,14 @@
 }
 */
 
+- (IBAction)loginAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    //获取要跳转过去的那个页面
+    UINavigationController *signNavi = [Utilities getStoryboardInstance:@"Member" byIdentity:@"SignNavi"];
+    [self presentViewController:signNavi animated:YES completion:nil];
+    
+    //执行跳转
+}
+
+- (IBAction)settingAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
 @end
